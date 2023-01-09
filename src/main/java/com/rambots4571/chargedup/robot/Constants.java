3205c8744e3216cb335cdcf89623c35d3761aff0
WIng57 <ddariusd007@gmail.com>
2023-01-09 01:23:29 -0500

    adding all 4 modules and pigeon IMU

--- src/main/java/com/rambots4571/chargedup/robot/Constants.java
+++ src/main/java/com/rambots4571/chargedup/robot/Constants.java
@@ -4,12 +4,30 @@
 
 package com.rambots4571.chargedup.robot;
 
-/**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
- *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
- */
-public final class Constants {}
+public final class Constants {
+
+  public static class DriveConstants {
+    // TODO: Find steer offsets
+    public static final int FRONT_LEFT_DRIVE_MOTOR = 1;
+    public static final int FRONT_LEFT_STEER_MOTOR = 2;
+    public static final int FRONT_LEFT_STEER_ENCODER = 3;
+    public static final double FRONT_LEFT_STEER_OFFSET = 0;
+
+    public static final int FRONT_RIGHT_DRIVE_MOTOR = 4;
+    public static final int FRONT_RIGHT_STEER_MOTOR = 5;
+    public static final int FRONT_RIGHT_STEER_ENCODER = 6;
+    public static final double FRONT_RIGHT_STEER_OFFSET = 0;
+
+    public static final int BACK_LEFT_DRIVE_MOTOR = 7;
+    public static final int BACK_LEFT_STEER_MOTOR = 8;
+    public static final int BACK_LEFT_STEER_ENCODER = 9;
+    public static final double BACK_LEFT_STEER_OFFSET = 0;
+
+    public static final int BACK_RIGHT_DRIVE_MOTOR = 10;
+    public static final int BACK_RIGHT_STEER_MOTOR = 11;
+    public static final int BACK_RIGHT_STEER_ENCODER = 12;
+    public static final double BACK_RIGHT_STEER_OFFSET = 0;
+
+    public static final int PIGEON_IMU_2 = 13;
+  }
+}
---
+++ src/main/java/com/rambots4571/chargedup/robot/subsystems/DriveTrain.java
@@ -0,0 +1,63 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package com.rambots4571.chargedup.robot.subsystems;
+
+import com.ctre.phoenix.sensors.Pigeon2;
+
+import com.rambots4571.chargedup.robot.Constants.DriveConstants;
+
+import com.swervedrivespecialties.swervelib.Mk4iSwerveModuleHelper;
+import com.swervedrivespecialties.swervelib.SwerveModule;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DriveTrain extends SubsystemBase {
+
+  private final SwerveModule frontLeft, frontRight, backLeft, backRight;
+
+  private final Pigeon2 gyro;
+
+  public DriveTrain() {
+    frontLeft =
+        Mk4iSwerveModuleHelper.createFalcon500(
+            Mk4iSwerveModuleHelper.GearRatio.L2,
+            DriveConstants.FRONT_LEFT_DRIVE_MOTOR,
+            DriveConstants.FRONT_LEFT_STEER_MOTOR,
+            DriveConstants.FRONT_LEFT_STEER_ENCODER,
+            DriveConstants.FRONT_LEFT_STEER_OFFSET);
+
+    frontRight =
+        Mk4iSwerveModuleHelper.createFalcon500(
+            Mk4iSwerveModuleHelper.GearRatio.L2,
+            DriveConstants.FRONT_RIGHT_DRIVE_MOTOR,
+            DriveConstants.FRONT_RIGHT_STEER_MOTOR,
+            DriveConstants.FRONT_RIGHT_STEER_ENCODER,
+            DriveConstants.FRONT_RIGHT_STEER_OFFSET);
+
+    backLeft =
+        Mk4iSwerveModuleHelper.createFalcon500(
+            Mk4iSwerveModuleHelper.GearRatio.L2,
+            DriveConstants.BACK_LEFT_DRIVE_MOTOR,
+            DriveConstants.BACK_LEFT_STEER_MOTOR,
+            DriveConstants.BACK_LEFT_STEER_ENCODER,
+            DriveConstants.BACK_LEFT_STEER_OFFSET);
+
+    backRight =
+        Mk4iSwerveModuleHelper.createFalcon500(
+            Mk4iSwerveModuleHelper.GearRatio.L2,
+            DriveConstants.BACK_RIGHT_DRIVE_MOTOR,
+            DriveConstants.BACK_RIGHT_STEER_MOTOR,
+            DriveConstants.BACK_RIGHT_STEER_ENCODER,
+            DriveConstants.BACK_RIGHT_STEER_OFFSET);
+
+    gyro = new Pigeon2(DriveConstants.PIGEON_IMU_2);
+  }
+
+  @Override
+  public void periodic() {}
+
+  @Override
+  public void simulationPeriodic() {}
+}
