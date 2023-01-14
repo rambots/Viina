1db535583b4216e5e65ad2cc7d88864c743227b3
mahim <mahim216@gmail.com>
2023-01-14 01:05:51 -0500

    using @Builder

--- src/main/java/com/rambots4571/chargedup/robot/Constants.java
+++ src/main/java/com/rambots4571/chargedup/robot/Constants.java
@@ -5,9 +5,9 @@
 package com.rambots4571.chargedup.robot;
 
 import com.rambots4571.chargedup.robot.utils.Alert;
+import com.rambots4571.chargedup.robot.utils.Alert.AlertType;
 import com.rambots4571.chargedup.robot.utils.COTSFalconSwerveConstants;
 import com.rambots4571.chargedup.robot.utils.SwerveModuleConstants;
-import com.rambots4571.chargedup.robot.utils.Alert.AlertType;
 
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -20,59 +20,59 @@ public final class Constants {
 
   public static class DriveConstants {
     ///////////////// CAN IDs /////////////////
-    
+
     /* Front Left Module - Module 0 */
-    public static final class Mod0 { //TODO: This must be tuned to specific robot
-        public static final int driveMotorID = 1;
-        public static final int angleMotorID = 2;
-        public static final int canCoderID = 3;
-        public static final Rotation2d angleOffset = Rotation2d.fromDegrees(0.0);
-        public static final SwerveModuleConstants constants = 
-            new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
+    public static final class Mod0 { // TODO: This must be tuned to specific robot
+      public static final int driveMotorID = 1;
+      public static final int angleMotorID = 2;
+      public static final int canCoderID = 3;
+      public static final Rotation2d angleOffset = Rotation2d.fromDegrees(0.0);
+      public static final SwerveModuleConstants constants =
+          new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
     }
 
     /* Front Right Module - Module 1 */
-    public static final class Mod1 { //TODO: This must be tuned to specific robot
-        public static final int driveMotorID = 4;
-        public static final int angleMotorID = 5;
-        public static final int canCoderID = 6;
-        public static final Rotation2d angleOffset = Rotation2d.fromDegrees(0.0);
-        public static final SwerveModuleConstants constants = 
-            new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
+    public static final class Mod1 { // TODO: This must be tuned to specific robot
+      public static final int driveMotorID = 4;
+      public static final int angleMotorID = 5;
+      public static final int canCoderID = 6;
+      public static final Rotation2d angleOffset = Rotation2d.fromDegrees(0.0);
+      public static final SwerveModuleConstants constants =
+          new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
     }
-    
+
     /* Back Left Module - Module 2 */
-    public static final class Mod2 { //TODO: This must be tuned to specific robot
-        public static final int driveMotorID = 7;
-        public static final int angleMotorID = 8;
-        public static final int canCoderID = 9;
-        public static final Rotation2d angleOffset = Rotation2d.fromDegrees(0.0);
-        public static final SwerveModuleConstants constants = 
-            new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
+    public static final class Mod2 { // TODO: This must be tuned to specific robot
+      public static final int driveMotorID = 7;
+      public static final int angleMotorID = 8;
+      public static final int canCoderID = 9;
+      public static final Rotation2d angleOffset = Rotation2d.fromDegrees(0.0);
+      public static final SwerveModuleConstants constants =
+          new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
     }
 
     /* Back Right Module - Module 3 */
-    public static final class Mod3 { //TODO: This must be tuned to specific robot
+    public static final class Mod3 { // TODO: This must be tuned to specific robot
       public static final int driveMotorID = 10;
       public static final int angleMotorID = 11;
       public static final int canCoderID = 12;
       public static final Rotation2d angleOffset = Rotation2d.fromDegrees(0.0);
-      public static final SwerveModuleConstants constants = 
+      public static final SwerveModuleConstants constants =
           new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
     }
 
     public static final int PIGEON_IMU_2 = 13;
 
     ///////////////// DRIVETRAIN CHARACTERISTICS /////////////////
-    public static final COTSFalconSwerveConstants chosenModule = 
-      COTSFalconSwerveConstants.SDSMK4i(COTSFalconSwerveConstants.driveGearRatios.SDSMK4i_L2);
+    public static final COTSFalconSwerveConstants chosenModule =
+        COTSFalconSwerveConstants.SDSMK4i(COTSFalconSwerveConstants.driveGearRatios.SDSMK4i_L2);
 
     public static final double kTrackWidthMeters = 0.42;
     public static final double kWheelBaseMeters = 0.73;
-    public static final double kWheelCircumference = chosenModule.wheelCircumference;
+    public static final double kWheelCircumference = chosenModule.getCircumference();
 
-    public static final double kDriveGearRatio = chosenModule.driveGearRatio;
-    public static final double kTurnGrearRatio = chosenModule.angleGearRatio;
+    public static final double kDriveGearRatio = chosenModule.getDriveGearRatio();
+    public static final double kTurnGrearRatio = chosenModule.getAngleGearRatio();
 
     public static final double kMaxSpeedMetersPerSecond = 3.0;
 
@@ -82,8 +82,7 @@ public final class Constants {
     public static final double kV = 1.98;
     public static final double kA = 0.2;
 
-    public static final SimpleMotorFeedforward swerveFF = 
-      new SimpleMotorFeedforward(kS, kV, kA);
+    public static final SimpleMotorFeedforward swerveFF = new SimpleMotorFeedforward(kS, kV, kA);
 
     public static final SwerveDriveKinematics kDriveKinematics =
         new SwerveDriveKinematics(
@@ -95,8 +94,8 @@ public final class Constants {
             new Translation2d(-kTrackWidthMeters / 2.0, kWheelBaseMeters / 2.0),
             // Back right
             new Translation2d(-kTrackWidthMeters / 2.0, -kWheelBaseMeters / 2.0));
-    }
-  
+  }
+
   public static class Settings {
     ///////////////// ADVANTAGE KIT LOGGING /////////////////
 
