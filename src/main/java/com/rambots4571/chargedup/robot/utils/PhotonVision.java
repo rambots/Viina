b015f491b4de3c1a142a84eea7279d5c978fb4c1
Wing57 <ddariusd007@gmail.com>
2023-01-26 17:04:13 -0500

    adding distance to target getter

--- src/main/java/com/rambots4571/chargedup/robot/utils/PhotonVision.java
+++ src/main/java/com/rambots4571/chargedup/robot/utils/PhotonVision.java
@@ -1,11 +1,15 @@
 package com.rambots4571.chargedup.robot.utils;
 
+import com.rambots4571.chargedup.robot.Constants.Settings;
+
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.util.sendable.Sendable;
 import edu.wpi.first.util.sendable.SendableBuilder;
-import edu.wpi.first.util.sendable.SendableRegistry;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import java.util.List;
 import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonUtils;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
@@ -18,7 +22,7 @@ public class PhotonVision implements Sendable {
   private static PhotonVision instance = new PhotonVision();
 
   private PhotonVision() {
-    SendableRegistry.addLW(this, "PhotonVision");
+    SmartDashboard.putData(this);
   }
 
   public static PhotonVision getInstance() {
@@ -41,7 +45,18 @@ public class PhotonVision implements Sendable {
     return result.getBestTarget();
   }
 
-  public Sendable getSendable(PhotonTrackedTarget target) {
+  public double getEstimatedDistanceToTarget() {
+    double estimatedDistance =
+        PhotonUtils.calculateDistanceToTargetMeters(
+            Settings.CAMERA_HEIGHT_METERS,
+            Settings.TARGET_HEIGHT_METERS,
+            Settings.CAMERA_PITCH_RADIANS,
+            Units.degreesToRadians(getBestTarget().getPitch()));
+
+    return estimatedDistance;
+  }
+
+  public Sendable getTargetSendable(PhotonTrackedTarget target) {
     return builder -> {
       builder.addDoubleProperty("Yaw", target::getYaw, null);
       builder.addDoubleProperty("Pitch", target::getPitch, null);
@@ -55,6 +70,7 @@ public class PhotonVision implements Sendable {
   public void initSendable(SendableBuilder builder) {
     builder.setSmartDashboardType("PhotonVision Values");
     builder.addBooleanProperty("Has Valid Targets", this::hasValidTarget, null);
-    getSendable(getBestTarget()).initSendable(builder);
+    builder.addDoubleProperty("Estimated Distance", this::getEstimatedDistanceToTarget, null);
+    getTargetSendable(getBestTarget()).initSendable(builder);
   }
 }
