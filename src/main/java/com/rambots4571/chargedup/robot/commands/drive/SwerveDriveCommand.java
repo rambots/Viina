6987b053d2ee27d5455c34cad828416885df1ac0
mahim <mahim216@gmail.com>
2023-01-29 21:21:49 -0500

    making sure packages are lowercase

---
+++ src/main/java/com/rambots4571/chargedup/robot/commands/drive/SwerveDriveCommand.java
@@ -0,0 +1,52 @@
+package com.rambots4571.chargedup.robot.commands.drive;
+
+import com.rambots4571.chargedup.robot.Constants.DriveConstants;
+import com.rambots4571.chargedup.robot.Constants.Settings;
+import com.rambots4571.chargedup.robot.subsystems.DriveTrain;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+import java.util.function.BooleanSupplier;
+import java.util.function.DoubleSupplier;
+
+public class SwerveDriveCommand extends CommandBase {
+
+  private DriveTrain driveTrain;
+
+  private DoubleSupplier translationSupplier, strafeSupplier, rotationSupplier;
+  private BooleanSupplier robotCentricSupplier;
+
+  public SwerveDriveCommand(
+      DriveTrain driveTrain,
+      DoubleSupplier translationSupplier,
+      DoubleSupplier strafeSupplier,
+      DoubleSupplier rotationSupplier,
+      BooleanSupplier robotCentricSupplier) {
+    this.driveTrain = driveTrain;
+    addRequirements(driveTrain);
+
+    this.translationSupplier = translationSupplier;
+    this.strafeSupplier = strafeSupplier;
+    ;
+    this.rotationSupplier = rotationSupplier;
+    this.robotCentricSupplier = robotCentricSupplier;
+  }
+
+  @Override
+  public void execute() {
+    double translationVal =
+        MathUtil.applyDeadband(translationSupplier.getAsDouble(), Settings.STICK_DEADBAND);
+    double strafeVal =
+        MathUtil.applyDeadband(strafeSupplier.getAsDouble(), Settings.STICK_DEADBAND);
+    double rotationVal =
+        MathUtil.applyDeadband(rotationSupplier.getAsDouble(), Settings.STICK_DEADBAND);
+
+    driveTrain.drive(
+        new Translation2d(translationVal, strafeVal).times(DriveConstants.kMaxSpeedMetersPerSecond),
+        (rotationVal * DriveConstants.kMaxAngularSpeedMetersPerSecond),
+        robotCentricSupplier.getAsBoolean(),
+        true);
+  }
+}
