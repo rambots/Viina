aec6f73f2c4a498c6560247ab5f5e1ad1f25dbce
mahim <mahim216@gmail.com>
2023-02-16 23:27:32 -0500

    setting voltage control

--- src/main/java/com/rambots4571/chargedup/robot/subsystems/Arm.java
+++ src/main/java/com/rambots4571/chargedup/robot/subsystems/Arm.java
@@ -1,5 +1,7 @@
 package com.rambots4571.chargedup.robot.subsystems;
 
+import com.rambots4571.rampage.motor.TalonPID;
+
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
 import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
@@ -7,7 +9,6 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
 import com.rambots4571.chargedup.robot.Constants.ArmConstants;
 import com.rambots4571.chargedup.robot.Constants.Settings;
-import com.rambots4571.rampage.motor.TalonPID;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -63,12 +64,26 @@ public class Arm extends SubsystemBase {
     armMotor.configMotionAcceleration(ArmConstants.motionAccel, Settings.timeoutMs);
   }
 
+  /**
+   * Set the length you want the arm to extend
+   *
+   * @param raw encoder ticks
+   */
   public void setLength(double raw) {
     armMotor.set(TalonFXControlMode.MotionMagic, raw);
   }
 
+  /**
+   * Updates the desired length which is set inside periodic()`
+   *
+   * @param desiredLength
+   */
   public void setDesiredLength(double desiredLength) {
     this.desiredLength = desiredLength;
+  }
+
+  public void setPower(double value) {
+    armMotor.set(value);
   }
 
   public void stop() {
